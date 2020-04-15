4988b2b354729ae688ebfc6d07cbcd79c2da6b5c
zamysel <zamysel@yandex-team.ru>
2020-04-15 18:08:05 +0300

    cleanup

--- wrappers/yandexcloudkms/yandexcloudkms.go
+++ wrappers/yandexcloudkms/yandexcloudkms.go
@@ -12,15 +12,13 @@ import (
 	ycsdk "github.com/yandex-cloud/go-sdk"
 )
 
-// These constants contain the accepted env vars
 const (
+	// Accepted env vars
 	EnvYandexCloudOAuthToken            = "YANDEXCLOUD_OAUTH_TOKEN"
 	EnvYandexCloudServiceAccountKeyFile = "YANDEXCLOUD_SERVICE_ACCOUNT_KEY_FILE"
 	EnvYandexCloudKMSKeyID              = "YANDEXCLOUD_KMS_KEY_ID"
-)
 
-// These constants contain the accepted config parameters
-const (
+	// Accepted config parameters
 	CfgYandexCloudOAuthToken            = "oauth_token"
 	CfgYandexCloudServiceAccountKeyFile = "service_account_key_file"
 	CfgYandexCloudKMSKeyID              = "kms_key_id"
@@ -55,22 +53,20 @@ func NewWrapper(opts *wrapping.WrapperOptions) *Wrapper {
 // Order of precedence Yandex.Cloud values:
 // * Environment variable
 // * Value from Vault configuration file
-// * Instance metadata role
-// * Default values
+// * Compute Instance metadata
 func (k *Wrapper) SetConfig(config map[string]string) (map[string]string, error) {
 	if config == nil {
 		config = map[string]string{}
 	}
 
 	// Check and set KeyID
-	keyID := coalesce(os.Getenv(EnvYandexCloudKMSKeyID), config[CfgYandexCloudKMSKeyID])
-	if keyID == "" {
+	k.keyID = coalesce(os.Getenv(EnvYandexCloudKMSKeyID), config[CfgYandexCloudKMSKeyID])
+	if k.keyID == "" {
 		return nil, fmt.Errorf(
-			"Neither '%s' environment variable nor '%s' config parameter is set",
+			"neither '%s' environment variable nor '%s' config parameter is set",
 			EnvYandexCloudKMSKeyID, CfgYandexCloudKMSKeyID,
 		)
 	}
-	k.keyID = keyID
 
 	// Check and set k.client
 	if k.client == nil {
@@ -79,11 +75,11 @@ func (k *Wrapper) SetConfig(config map[string]string) (map[string]string, error)
 			coalesce(os.Getenv(EnvYandexCloudServiceAccountKeyFile), config[CfgYandexCloudServiceAccountKeyFile]),
 		)
 		if err != nil {
-			return nil, fmt.Errorf("error initializing Yandex.Cloud KMS wrapping client: %w", err)
+			return nil, fmt.Errorf("error initializing Yandex.Cloud KMS client: %w", err)
 		}
 
 		if err := k.setClient(client); err != nil {
-			return nil, err
+			return nil, fmt.Errorf("error setting Yandex.Cloud KMS client: %w", err)
 		}
 	}
 
@@ -97,12 +93,15 @@ func (k *Wrapper) SetConfig(config map[string]string) (map[string]string, error)
 func (k *Wrapper) setClient(client kms.SymmetricCryptoServiceClient) error {
 	k.client = client
 
-	// Make sure user has permissions to encrypt (also checks if key exists)
-	if _, err := k.Encrypt(context.Background(), []byte("go-kms-wrapping-test"), nil); err != nil {
+	// Make sure all the required permissions are granted (also checks if key exists)
+	out, err := k.Encrypt(context.Background(), []byte("go-kms-wrapping-test"), nil)
+	if err != nil {
 		return fmt.Errorf(
-			"failed to encrypt with Yandex.Cloud KMS key - ensure the "+
-				"key exists and the service account (or the user) has all"+
-				"the required permissions: %w", err)
+			"failed to encrypt with Yandex.Cloud KMS key - ensure the key exists and permission to encrypt the key is granted: %w", err)
+	}
+	if _, err := k.Decrypt(context.Background(), out, nil); err != nil {
+		return fmt.Errorf(
+			"failed to decrypt with Yandex.Cloud KMS key - ensure the key exists and permission to decrypt the key is granted: %w", err)
 	}
 
 	return nil
@@ -170,7 +169,7 @@ func (k *Wrapper) Encrypt(_ context.Context, plaintext, aad []byte) (blob *wrapp
 		IV:         env.IV,
 		KeyInfo: &wrapping.KeyInfo{
 			// Even though we do not use the version id during decryption, store it
-			// to know exactly the specific key used in encryption in case we
+			// to know exactly the specific key version used in encryption in case we
 			// want to rewrap older entries
 			KeyID:      encryptResponse.VersionId,
 			WrappedKey: encryptResponse.Ciphertext,
@@ -214,7 +213,7 @@ func (k *Wrapper) Decrypt(_ context.Context, in *wrapping.EncryptedBlobInfo, aad
 func getYandexCloudKMSClient(oauthToken string, serviceAccountKeyFile string) (kms.SymmetricCryptoServiceClient, error) {
 	credentials, err := getCredentials(oauthToken, serviceAccountKeyFile)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error getting credentials: %w", err)
 	}
 
 	sdk, err := ycsdk.Build(
@@ -222,7 +221,7 @@ func getYandexCloudKMSClient(oauthToken string, serviceAccountKeyFile string) (k
 		ycsdk.Config{Credentials: credentials},
 	)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error building Yandex.Cloud SDK instance: %w", err)
 	}
 
 	return sdk.KMSCrypto().SymmetricCrypto(), nil
@@ -230,21 +229,24 @@ func getYandexCloudKMSClient(oauthToken string, serviceAccountKeyFile string) (k
 
 func getCredentials(oauthToken string, serviceAccountKeyFile string) (ycsdk.Credentials, error) {
 	if oauthToken != "" && serviceAccountKeyFile != "" {
-		return nil, fmt.Errorf("TODO")
+		return nil, fmt.Errorf("error configuring authentication: both OAuth token and service account key file are specified")
 	}
 
+	// Yandex account authentification (via Oauth token)
 	if oauthToken != "" {
 		return ycsdk.OAuthToken(oauthToken), nil
 	}
 
+	// Service account authentication (via authorized key)
 	if serviceAccountKeyFile != "" {
 		key, err := iamkey.ReadFromJSONFile(serviceAccountKeyFile)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("error reading service account key file: %w", err)
 		}
 		return ycsdk.ServiceAccountKey(key)
 	}
 
+	// Compute Instance Service Account authentication
 	return ycsdk.InstanceServiceAccount(), nil
 }
 
