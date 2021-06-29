238f4f67065e20f0ac52cdc373a753135c8ee1a2
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-06-29 11:04:33 -0400

    Put wrapper options back to a struct

--- options.go
+++ options.go
@@ -1,5 +1,7 @@
 package wrapping
 
+import structpb "google.golang.org/protobuf/types/known/structpb"
+
 // GetOpts iterates the inbound Options and returns a struct
 func GetOpts(opt ...Option) Options {
 	opts := getDefaultOptions()
@@ -20,7 +22,7 @@ type Options struct {
 	WithAad            []byte
 	WithKeyId          string
 	WithKeyNotRequired bool
-	WithWrapperOptions map[string]string
+	WithWrapperOptions *structpb.Struct
 }
 
 func getDefaultOptions() Options {
@@ -51,7 +53,7 @@ func WithKeyNotRequired(notRequired bool) Option {
 
 // WithWrapperOptions is an option accepted by wrappers at configuration time
 // and/or in other function calls to control wrapper-specific behavior.
-func WithWrapperOptions(options map[string]string) Option {
+func WithWrapperOptions(options *structpb.Struct) Option {
 	return func(o *Options) {
 		o.WithWrapperOptions = options
 	}
