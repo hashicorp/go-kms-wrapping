b61e8e89a78cb817e9162b74ea46b410d915afd7
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-07-27 11:43:55 -0400

    Update plugin main

--- wrappers/aead/plugin/main.go
+++ wrappers/aead/plugin/main.go
@@ -1,16 +1,23 @@
-package plugin
+package main
 
 import (
 	gkwp "github.com/hashicorp/go-kms-wrapping/plugin"
 	"github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2"
+	"github.com/hashicorp/go-plugin"
 	gp "github.com/hashicorp/go-plugin"
 )
 
+var HandshakeConfig = plugin.HandshakeConfig{
+	MagicCookieKey:   "HASHICORP_GKMS_AEAD_PLUGIN",
+	MagicCookieValue: "Hi there!",
+}
+
 func main() {
 	gp.Serve(&gp.ServeConfig{
+		HandshakeConfig: HandshakeConfig,
 		VersionedPlugins: map[int]gp.PluginSet{
 			1: gp.PluginSet{
-				"wrapping": &gkwp.NewWrapper(aead.NewWrapper()),
+				"wrapping": gkwp.NewWrapper(aead.NewWrapper()),
 			},
 		},
 		GRPCServer: gp.DefaultGRPCServer,
