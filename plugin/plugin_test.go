838594514cd1efdaf0a6f39a56e969207062edbe
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-08-21 17:42:20 -0400

    Update test

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -59,7 +59,8 @@ func TestAeadPluginWrapper(t *testing.T) {
 }
 
 func TestInterfaces(t *testing.T) {
-	assert := assert.New(t)
+	require, assert := require.New(t), assert.New(t)
+	ctx := context.Background()
 
 	pluginPath := os.Getenv("PLUGIN_PATH")
 	if pluginPath == "" {
@@ -67,17 +68,29 @@ func TestInterfaces(t *testing.T) {
 	}
 
 	var ok bool
+
+	// First get a normal wrapper and ensure it doesn't satisfy InitFinalizer
 	wrapper, wrapperCleanup := TestPlugin(t, filepath.Join(pluginPath, "wrapperplugin"))
 	if wrapperCleanup != nil {
 		defer wrapperCleanup()
 	}
+	keyId, err := wrapper.KeyId(ctx)
+	assert.NoError(err)
+	assert.Equal(keyId, "static-key")
 	_, ok = wrapper.(wrapping.InitFinalizer)
 	assert.False(ok)
 
+	// Now get a wrapper satisfying InitFinalizer and ensure it does
 	initFinalizer, initFinalizerCleanup := TestPlugin(t, filepath.Join(pluginPath, "initfinalizerplugin"), WithInitFinalizeInterface(true))
 	if initFinalizerCleanup != nil {
 		defer initFinalizerCleanup()
 	}
-	_, ok = initFinalizer.(wrapping.InitFinalizer)
-	assert.True(ok)
+	keyId, err = initFinalizer.KeyId(ctx)
+	assert.NoError(err)
+	assert.Equal(keyId, "static-key")
+
+	ifWrapper, ok := initFinalizer.(wrapping.InitFinalizer)
+	require.True(ok)
+	require.NoError(ifWrapper.Init(ctx))
+	require.NoError(ifWrapper.Finalize(ctx))
 }
