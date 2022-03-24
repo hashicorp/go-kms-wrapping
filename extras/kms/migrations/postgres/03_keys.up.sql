15f64f21d18bfea303a0ea9ead3b0f38498922c8
Jim <jlambert@hashicorp.com>
2022-03-24 11:00:50 -0700

    fix: address issues in initial schemas

--- extras/kms/migrations/postgres/03_keys.up.sql
+++ extras/kms/migrations/postgres/03_keys.up.sql
@@ -1,45 +1,31 @@
 begin;
 
+
+-- kms_version_column() will increment the version column whenever row data
+-- is inserted and should only be used in an before insert trigger.  This
+-- function will overwrite any explicit values to the version column.
 create or replace function
-  kms_root_key_version_column()
+  kms_version_column()
   returns trigger
 as $$
+declare 
+  _key_id text;
+  _max bigint; 
 begin
-  update kms_root_key_version set version =
-  (
-    select max(coalesce(version,0)) + 1 
-    from kms_root_key_version 
-    where 
-      data_key_id = new.data_key_id 
-  )
-  where rowid = new.rowid;  
+  execute format('SELECT $1.%I', tg_argv[0]) into _key_id using new;
+  execute format('select max(version) + 1 from %I where %I = $1', tg_relid::regclass, tg_argv[0]) using _key_id into _max;
+  if _max is null then
+  	_max = 1;
+  end if;
+  new.version = _max;
+  return new;
 end;
 $$ language plpgsql;
-comment on function
-  kms_root_key_version_column()
-is
-  'function used in before insert trigger on kms_root_key_version to properly set version column';
 
-
-create or replace function
-  kms_data_key_version_column()
-  returns trigger
-as $$
-begin
-  update kms_data_key_version set version =
-  (
-    select max(coalesce(version,0)) + 1 
-    from kms_data_key_version 
-    where 
-      data_key_id = new.data_key_id and  
-      purpose = new.purpose
-  )
-  where rowid = new.rowid;  
-end;
-$$ language plpgsql;
 comment on function
-  kms_data_key_version_column()
+  kms_version_column()
 is
-  'function used in before insert trigger on kms_data_key_version to properly set version column';
+  'function used in before insert triggers to properly set version columns for kms_* tables with a version column';
+  
 
 commit;
