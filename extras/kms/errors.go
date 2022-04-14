6da73d27a96959dfa31ebe5fd818b5775de254b5
Jim <jlambert@hashicorp.com>
2022-04-14 13:28:01 -0400

    fixup! feature: begin building out extras/kms module (#66)

--- extras/kms/errors.go
+++ extras/kms/errors.go
@@ -7,7 +7,7 @@ var (
 	// doesn't match the require version of the module.
 	ErrInvalidVersion = errors.New("invalid version")
 
-	// ErrInvalidParameter represents and invalid parameter error condition.
+	// ErrInvalidParameter represents an invalid parameter error condition.
 	ErrInvalidParameter = errors.New("invalid parameter")
 
 	// ErrMultipleRecords represents multiple records were affected when only
